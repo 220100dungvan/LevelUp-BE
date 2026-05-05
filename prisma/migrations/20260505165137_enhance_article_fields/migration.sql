1a3f9297827aaec494ebf0ac763f3e4969624e7e
Quyt <dungvan.170204@gmail.com>
2026-05-05 23:52:44 +0700

    feat(database): enhance Article model

---
+++ prisma/migrations/20260505165137_enhance_article_fields/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "articles" ADD COLUMN     "content_vi" TEXT,
+ADD COLUMN     "source_url" TEXT,
+ADD COLUMN     "thumbnail_url" TEXT,
+ADD COLUMN     "voice_type" "VoiceType";
