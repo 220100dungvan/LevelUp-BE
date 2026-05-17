edc0a9cf57ddc08a52f5cd6f611431ed5012825d
Quyt <dungvan.170204@gmail.com>
2026-05-18 03:37:58 +0700

    feat(database): add description, invite code, and image URL fields to Class model

---
+++ prisma/migrations/20260517203436_add_invite_code_feilds_for_class/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[invite_code]` on the table `classes` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `invite_code` to the `classes` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "classes" ADD COLUMN     "description" TEXT,
+ADD COLUMN     "image_url" TEXT,
+ADD COLUMN     "invite_code" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "classes_invite_code_key" ON "classes"("invite_code");
